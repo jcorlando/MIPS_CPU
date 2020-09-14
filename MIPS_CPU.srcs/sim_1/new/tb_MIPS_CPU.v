6e8676108f5ef22d2c74faea5860ab1a00622144
Juan Orlando <jcorlandomc@yahoo.com>
2020-09-13 23:56:22 -0700

    9/13/2020 11:56 pm

--- MIPS_CPU.srcs/sim_1/new/tb_MIPS_CPU.v
+++ MIPS_CPU.srcs/sim_1/new/tb_MIPS_CPU.v
@@ -4,14 +4,14 @@ module tb_MIPS_CPU;
 // Parameters
 parameter WL = 32, MEM_Depth = 64;
 //Inputs
-reg CLK;                            // Clock
-reg DMWE;                           // Data Memory
-reg [WL - 1 : 0] DMA;               // Data Memory
-reg [WL - 1 : 0] DMWD;              // Data Memory
-reg [WL - 1 : 0] addr;              // Instruction Memory
+reg CLK;                                    // Clock
+reg DMWE;                                   // Data Memory
+reg [WL - 1 : 0] DMA;                       // Data Memory
+reg [WL - 1 : 0] DMWD;                      // Data Memory
+reg [WL - 1 : 0] addr;                      // Instruction Memory
 // Outputs
-wire [WL - 1 : 0] DMRD;             // Data Memory
-wire [WL - 1 : 0] instruction;      // Instruction Memory
+wire [WL - 1 : 0] DMRD;                     // Data Memory
+wire [WL - 1 : 0] instruction;              // Instruction Memory
 // Instantiate DUT
     top # ( .WL(WL), .MEM_Depth(MEM_Depth) )
             DUT( .CLK(CLK),                 // Clock
@@ -23,20 +23,25 @@ wire [WL - 1 : 0] instruction;      // Instruction Memory
                  .instruction(instruction)  // Instruction Memory
                );
 // Clock generation
-always #125 CLK <= ~CLK;
+always #100 CLK <= ~CLK;
 initial
     begin
-        CLK <= 0;                   // Clock
-        DMWD <= 0;                  // Data Memory
-        DMWE <= 0;                  // Data Memory
-        DMA  <= 32'h00000000;       // Data Memory
-        @(posedge CLK);             // Clock
-        DMA  <= 32'h00000001;       // Data Memory
-        @(posedge CLK);             // Clock
-        DMA  <= 32'h00000002;       // Data Memory
-        @(posedge CLK);             // Clock
-        DMA  <= 32'h00000003;       // Data Memory
-        @(posedge CLK);             // Clock
-        DMA  <= 32'h00000004;       // Data Memory
+        CLK <= 0;                           // Clock
+        addr <= 0;                          // Instruction Memory
+        DMWD <= 0;                          // Data Memory
+        DMWE <= 0;                          // Data Memory
+        DMA  <= 0;                          // Data Memory
+        @(posedge CLK);                     // Clock
+        DMA  <= 1;                          // Data Memory
+        addr <= 1;                          // Instruction Memory
+        @(posedge CLK);                     // Clock
+        DMA  <= 2;                          // Data Memory
+        addr <= 2;                          // Instruction Memory
+        @(posedge CLK);                     // Clock
+        DMA  <= 3;                          // Data Memory
+        addr <= 3;                          // Instruction Memory
+        @(posedge CLK);                     // Clock
+        DMA  <= 4;                          // Data Memory
+        addr <= 4;                          // Instruction Memory
     end
 endmodule
